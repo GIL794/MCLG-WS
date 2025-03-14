51ce15f508cc95b2dec1fe906e69007ba9dd690f
Gabriele Langellotto <200216885+GIL794@users.noreply.github.com>
2025-03-14 14:30:31 +0000

    Update __init__.py
    Signed-off-by: Gabriele Langellotto <200216885+GIL794@users.noreply.github.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,48 @@
+"""
+Idea founded by Gabriele Iacopo Langellotto
+MCLG-WS Application Package
+"""
+__version__ = '0.1.0'
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Idea founded by Gabriele Iacopo Langellotto
